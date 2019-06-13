4545fa16a7816beae66ac6010d4a66de48cd7f4c
thomas michiels <thomas.michiels@external.atlascopco.com>
2019-06-13 16:49:54 +0200

    1high titlebar + somewhat better collor

--- middleware/titlebar.js
+++ middleware/titlebar.js
@@ -3,9 +3,9 @@ const customTitlebar = require('custom-electron-titlebar');
 export default function ({route, app}) {
   if(route.fullPath !== '/update'){
     const test = new customTitlebar.Titlebar({
-      backgroundColor: customTitlebar.Color.fromHex('#444'),
+      backgroundColor: customTitlebar.Color.fromHex('#2a4365'),
       titleHorizontalAlignment: "center",
-      menuPosition: "bottom"
+      menuPosition: "left"
     });
   }
 }
