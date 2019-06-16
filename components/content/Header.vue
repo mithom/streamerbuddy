d242320943a1537b5f00427f3036123aafac8c66
mi_thom <mi_thom@hotmail.com>
2019-06-16 17:04:32 +0200

    extract toggleButton

--- components/content/Header.vue
+++ components/content/Header.vue
@@ -12,13 +12,7 @@
                     d="M3.94 6.5L2.22 3.64l1.42-1.42L6.5 3.94c.52-.3 1.1-.54 1.7-.7L9 0h2l.8 3.24c.6.16 1.18.4 1.7.7l2.86-1.72 1.42 1.42-1.72 2.86c.3.52.54 1.1.7 1.7L20 9v2l-3.24.8c-.16.6-.4 1.18-.7 1.7l1.72 2.86-1.42 1.42-2.86-1.72c-.52.3-1.1.54-1.7.7L11 20H9l-.8-3.24c-.6-.16-1.18-.4-1.7-.7l-2.86 1.72-1.42-1.42 1.72-2.86c-.3-.52-.54-1.1-.7-1.7L0 11V9l3.24-.8c.16-.6.4-1.18.7-1.7zM10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
                 />
             </svg>
-            <span
-                class="border rounded-full border-blue-500 inline-block items-center float-right cursor-pointer w-12 bg-blue-600 justify-end"
-            >
-                <span
-                    class="rounded-full border w-6 h-6 border-blue-500 shadow-inner bg-white shadow block float-right"
-                />
-            </span>
+            <ToggleButton/>
             <button
                 class="leading-snug bg-blue-800 hover:bg-blue-900 text-white px-4 py-2 shadow-md uppercase font-semibold rounded-lg ml-4"
             >
@@ -29,8 +23,10 @@
 </template>
 
 <script>
+import ToggleButton from '../parts/ToggleButton'
 export default {
   name: 'Header',
+  components: {ToggleButton},
   data(){
     return {
       name: "StreamTimers"
---
+++ components/parts/ToggleButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <span
+        class="border rounded-full border-blue-500 inline-block items-center float-right cursor-pointer w-12 bg-blue-600 justify-end"
+    >
+        <span
+            class="rounded-full border w-6 h-6 border-blue-500 shadow-inner bg-white shadow block float-right"
+        />
+    </span>
+</template>
+
+<script>
+//TODO: toggle ability
+export default {
+  name: 'ToggleButton'
+}
+</script>
+
+<style scoped>
+
+</style>
