b29bb149df3ed1a2de43b7815b7c6971dadfe64b
mi_thom <mi_thom@hotmail.com>
2019-08-05 00:08:48 +0200

    consistent paths

--- plugins/component-plugin.js
+++ plugins/component-plugin.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Card from '~/components/parts/Card'
 import ToggleButton from '~/components/parts/ToggleButton'
-import Settings from '~/components/moduleParts/Settings'
-import Integer from "../components/ModuleParts/Settings/Integer";
-import Boolean from "../components/ModuleParts/Settings/Boolean";
-import String from "../components/ModuleParts/Settings/String";
-import Number from "../components/ModuleParts/Settings/Number";
-import Range from "../components/ModuleParts/Settings/Range";
-import SelectBox from "../components/ModuleParts/Settings/SelectBox";
-import Button from "../components/ModuleParts/Settings/Button";
-import ColorPicker from "../components/ModuleParts/Settings/ColorPicker";
+import Settings from '~/components/moduleParts/Settings/Settings'
+import Integer from "~/components/ModuleParts/Settings/Integer";
+import Boolean from "~/components/ModuleParts/Settings/Boolean";
+import String from "~/components/ModuleParts/Settings/String";
+import Number from "~/components/ModuleParts/Settings/Number";
+import Range from "~/components/ModuleParts/Settings/Range";
+import SelectBox from "~/components/ModuleParts/Settings/SelectBox";
+import Button from "~/components/ModuleParts/Settings/Button";
+import ColorPicker from "~/components/ModuleParts/Settings/ColorPicker";
 
 // register all components here that will be available to the dynamic modules
 Vue.component('Card', Card)
