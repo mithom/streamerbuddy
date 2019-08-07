d081ebdde7abdd31868228159196cd6eb1455d44
thomas michiels <thomas.michiels@external.atlascopco.com>
2019-08-07 14:38:36 +0200

    some styling attempt

--- components/ConnectionsManager/ConnectionsManager.vue
+++ components/ConnectionsManager/ConnectionsManager.vue
@@ -5,16 +5,42 @@
         height="75%"
     >
         <div class="overflow-auto h-full p-4">
-            <CloseButton @click.native="$modal.hide('connectionManager')" />
+            <CloseButton
+                class="z-10"
+                @click.native="$modal.hide('connectionManager')"
+            />
             <div class="flex flex-col">
                 <portal-target
                     name="connections"
                     :multiple="true"
                 />
-                <button class="hover:bg-gray-200 w-full p-2 border-b flex items-center">
-                    <span class="border border-blue-300 bg-gray-200 w-10 h-10 inline-block rounded plus" />
-                    <span class="px-2">
+                <!-- class="w-full p-2 border-b flex items-center relative" -->
+                <button
+                    class="w-full p-2 border-b relative text-left"
+                    :class="{'hover:bg-gray-200': !opened, 'cursor-default': opened}"
+                    :disabled="opened"
+                    @click="toggleAdd"
+                >
+                    <span class="align-middle border border-blue-300 bg-gray-200 w-10 h-10 inline-block rounded plus" />
+                    <span class="px-2 inline-block align-middle">
                         Add Connection
+                    </span>
+                    <span
+                        v-if="opened"
+                        class="block"
+                    >
+                        opened: {{ opened }}
+                        <InstallButton
+                            class="right-0 top-0 absolute mr-16 mt-2"
+                            :install="toggleAdd"
+                            @click.native.stop
+                        >
+                            Close
+                        </InstallButton>
+                        <portal-target
+                            name="addConnection"
+                            :multiple="true"
+                        />
                     </span>
                 </button>
             </div>
@@ -24,11 +50,23 @@
 
 <script>
 import CloseButton from "../parts/CloseButton";
+import InstallButton from "../parts/InstallButton";
+
 export default {
   name: "ConnectionsManager",
-  components: {CloseButton},
+  components: {InstallButton, CloseButton},
+  data(){
+    return {
+      opened: false
+    }
+  },
   mounted() {
     this.$modal.show('addConnection')
+  },
+  methods:{
+    toggleAdd: function(){
+      this.opened = !this.opened
+    }
   }
 }
 </script>
