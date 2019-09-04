84e6cf9aa052b1e52e6af357dbb51a604de097a4
mi_thom <mi_thom@hotmail.com>
2019-09-04 23:35:51 +0200

    add delete dropzone

--- components/content/Dashboard/Dashboard.vue
+++ components/content/Dashboard/Dashboard.vue
@@ -18,7 +18,7 @@
                 <draggable
                     v-model="myList"
                     class="flex flex-wrap"
-                    ghost-class="ghostclass"
+                    ghost-class="ghostClass"
                     :disabled="!managing"
                     :animation="150"
                     :group="{name:'componentList', pull: true, put: true}"
@@ -33,6 +33,24 @@
                         non default text
                     </component>
                 </draggable>
+                <div
+                    v-if="drag"
+                    class="rounded-full border border-red-500 w-12 h-12 mx-auto absolute left-0 right-0 bottom-0 mb-6 overflow-hidden"
+                >
+                    <svg
+                        class="h-full w-full fill-current text-red-400 absolute"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 21 21"
+                    >
+                        <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                    </svg>
+                    <draggable
+                        v-model="thrashList"
+                        :group="{name:'componentList', pull: false, put: true}"
+                        tag="div"
+                        class="bg-red-300 opacity-50 h-full w-full absolute top-0"
+                    />
+                </div>
             </div>
         </div>
     </div>
@@ -63,6 +81,10 @@ export default {
       set(value) {
         this.$store.commit('dashboard/updateList', value)
       }
+    },
+    thrashList: {
+      get(){ return []},
+      set(value){}
     }
   },
   methods:{
@@ -82,7 +104,7 @@ export default {
 
 </script>
 <style scoped>
-.ghostclass{
+.ghostClass{
     @apply bg-gray-300;
     @apply rounded-xxl;
     @apply border-4;
