a5a3c5a21b6875adc5733f30a2ee38da8135f97a
thomas michiels <thomas.michiels@external.atlascopco.com>
2019-09-24 14:26:52 +0200

    fix cache for module installation

--- app/myOctoKit.js
+++ app/myOctoKit.js
@@ -2,8 +2,15 @@ const throttling = require('@octokit/plugin-throttling')
 const retry = require('@octokit/plugin-retry')
 const Octokit = require('@octokit/rest')
 const app = require('electron').app || require('electron').remote.app
-
+const merge = require('lodash.merge')
 const MyOctokit = Octokit.plugin([retry, throttling]) //retry requests up to 3 times for recoverable errors
+const storage = require('electron-json-storage') //filesystem access only on remote thread
+const {promisify} = require('util')
+
+const get = promisify(storage.get)
+const set = promisify(storage.set)
+const remove = promisify(storage.remove)
+const has = promisify(storage.has)
 
 module.exports.owner = 'StreamerBuddy'
 module.exports.repo = 'streamerbuddy-store'
@@ -21,3 +28,37 @@ const octokit = MyOctokit({
 })
 
 module.exports.octokit = octokit
+
+let registered = false;
+
+//custom cache implementation
+function registerHooks(state, commit){
+  if(!registered){
+    octokit.hook.before('request', async (options)=>{
+      if(state.cache.Etags[options.url] && await has(state.cache.Etags[options.url])){
+        return merge(options, {headers:{'If-None-Match': state.cache.Etags[options.url]}})
+      }
+    })
+
+    octokit.hook.error('request', async (error, options)=>{
+      if(error.status === 304){
+        error.data = await get(error.headers.etag)
+      }
+      return error
+    })
+
+    octokit.hook.after('request', async (result, options)=>{
+      if(result.status ===200 && result.headers.etag){
+        if(result.url in state.cache.Etags){
+          await remove(state.cache.Etags[result.url])
+        }
+        await set(result.headers.etag, result.data)
+        commit('cache/saveEtag', {uri: result.url, etag: result.headers.etag}, {root: true})
+      }
+    })
+
+    registered = true
+  }
+}
+
+module.exports.registerHooks = registerHooks
--- store/moduleStore/index.js
+++ store/moduleStore/index.js
@@ -1,44 +1,5 @@
-const {octokit, owner, repo} = require('~/app/myOctoKit')
-const merge = require('lodash.merge')
-const storage = require('electron').remote.require('electron-json-storage') //filesystem access only on remote thread
-import {promisify} from 'util';
-
-const get = promisify(storage.get)
-const set = promisify(storage.set)
-const remove = promisify(storage.remove)
-
-
-let registered = false;
-
-//custom cache implementation
-function registerHooks(state, commit){
-  if(!registered){
-    octokit.hook.before('request', async (options)=>{
-      if(state.cache.Etags[options.url]){
-        return merge(options, {headers:{'If-None-Match': state.cache.Etags[options.url]}})
-      }
-    })
-
-    octokit.hook.error('request', async (error, options)=>{
-      if(error.status === 304){
-        error.data = await get(error.headers.etag)
-      }
-      return error
-    })
-
-    octokit.hook.after('request', async (result, options)=>{
-      if(result.status ===200 && result.headers.etag){
-        if(result.url in state.cache.Etags){
-          remove(state.cache.Etags[options.uri])
-        }
-        set(result.headers.etag, result.data)
-        commit('cache/saveEtag', {uri: result.url, etag: result.headers.etag}, {root: true})
-      }
-    })
-
-    registered = true
-  }
-}
+const {remote} = require('electron')
+const {octokit, owner, repo, registerHooks} = remote.require('./app/myOctoKit')
 
 async function getAvailableModuleFolders(state, commit){
   //TODO: ref -> master vs beta branch for beta testers
