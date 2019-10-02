a14a4dc4a3cf9632ab2bd5836227007e323a5739
thomas michiels <thomas.michiels@external.atlascopco.com>
2019-10-02 14:22:30 +0200

    fix crashing loading-screen + use promise for messagebox in updater

--- app/updater.js
+++ app/updater.js
@@ -52,29 +52,22 @@ exports.checkForUpdate = async function () {
   try {
     let updateCheck = await autoUpdater.checkForUpdates()
     if (autoUpdater.currentVersion < updateCheck.updateInfo.version) {
-      dialog.showMessageBox(exports.win,{
+      let messageBoxReturnValue = await dialog.showMessageBox(exports.win,{
         type: "question",
         buttons: ["Yes", "Cancel"],
         title: 'Update Available',
         message: "An update is available for download.\n do you want to download and install it now?"
-      }, async (response) => {
-        if (response === 0) {
-          await updateWin()
-          try {
-            await autoUpdater.downloadUpdate();
-
-            let sleep = function (ms) {
-              return new Promise(resolve => setTimeout(resolve, ms));
-            }
-
-            await sleep(1000)
-            autoUpdater.quitAndInstall(false, true)
-          } catch (e) {
-            console.log('failed to download update')
-            console.log(e)
-          }
-        } // user doesn't want to update
       })
+      if (messageBoxReturnValue.response === 0) {
+        await updateWin()
+        try {
+          await autoUpdater.downloadUpdate();
+          autoUpdater.quitAndInstall(false, true)
+        } catch (e) {
+          console.log('failed to download update')
+          console.log(e)
+        }
+      }
     } // no new version
   } catch (rejected) { // update server not available
     console.log('failed to check for update')
